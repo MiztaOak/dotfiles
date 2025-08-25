b21354d936288de39e8f507e2f3d9f27ca600266
Johan Ek <johan.ek@havochvatten.se>
2025-08-25 16:44:41 +0200

    Removev neo-tree and added telescope

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 require("config.harpoon_config")
+
+-- open file_browser with the path of the current buffer
+vim.keymap.set("n", "<space>e", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,4 @@
 return {
   { "akinosho/bufferline.nvim", enabled = false },
-  { "nvim-telescope/telescope.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
