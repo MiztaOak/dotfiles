c0e8a956b686204c5d79c87dcb48972b12c39237
Johan Ek <johan.ek@tuta.com>
2026-01-19 13:59:15 +0100

    Removed noctalia and updated dmenu script

--- scripts/dmenu.sh
+++ scripts/dmenu.sh
@@ -1,1 +1,1 @@
-dmenu_run -fn 'JetBrainsMono nerd Font-12' -i -l 10 -sb '#ebdbb2' -sf '#504945' -nf '#504945' -nb '#c9b890' -p 'Run:'
+dmenu_run -fn 'JetBrainsMono nerd Font-12' -i -l 10 -nb '#ebdbb2' -nf '#504945' -sf '#ebdbb2' -sb '#076678' -p 'Run:'
