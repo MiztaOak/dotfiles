4af3ec81e773c0a201b934f4f655c432473e64b5
Johan Ek <johan.ek@tuta.com>
2026-02-06 17:10:24 +0100

    couple of smaller changes

--- scripts/wmenu.sh
+++ scripts/wmenu.sh
@@ -1,1 +1,1 @@
-wmenu-run -i -f 'JetBrainsMono nerd Font 12' -l 10 -p 'Run' -S ebdbb2 -s d79921 -n 504945E6 -N c9b890 -M d79921 -m 282828
+pkill wmenu || wmenu-run -i -f 'JetBrainsMono nerd Font 12' -l 10 -p 'Run' -S 076678 -s ebdbb2 -n 504945E6 -N ebdbb2 -M 076678 -m ebdbb2
